de96e02aa93bedb83d8e1a064b284866880050a6
John Devine <jdevine103@gmail.com>
2020-09-24 14:46:59 -0400

    Add project files.

---
+++ DiscographyTracker.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DiscographyTracker.WebMVC.MvcApplication" Language="C#" %>
---
+++ DiscographyTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiscographyTracker.WebMVC.Startup))]
+namespace DiscographyTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
