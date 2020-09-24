29aa766ad6d359bc98dd59f303240575285e6f62
John Devine <jdevine103@gmail.com>
2020-09-24 15:32:52 -0400

    Data layer laid out

---
+++ DiscographyTracker.Data/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscographyTracker.Data
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistID { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "There are too many characters in this field.")]
+        [Display(Name = "Artist Name")]
+        public string ArtistName { get; set; }
+        public virtual List<Album> Albums { get; set; }
+    }
+}
